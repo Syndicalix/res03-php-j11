d624afae3a29f0ad5dc753c22ac7e433166be274
Syndicalix <speich.sebastien@gmail.com>
2023-02-09 11:57:31 +0000

    Exo Heritage MVC

---
+++ services/Router.php
@@ -0,0 +1,27 @@
+<?php 
+
+class Router
+{
+    private UserController $uc;
+    public function __construct()
+    {
+        $this->uc = new UserController();
+    }
+    public function checkRoute(string $route)
+    {
+        if($route === "users"){
+            $this->uc-> index();
+        }
+        else if($route === "user-create"){
+            $this->uc-> create();
+        }
+        else if($route === "user-edit"){
+            $this->uc-> edit();
+        }
+        else{
+            $this->uc->index();
+        }
+    }
+}
+
+?>
